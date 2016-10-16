138a5f9e9dfcc53ca3580c9fe66b7b807f2ff9a2
Miles Frankel <miles.frankel@gmail.com>
2016-10-16 13:21:11 -0400

    destroy metadata on destroy project

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,11 +2,15 @@ class Project < ApplicationRecord
   has_one :song
 
   after_save :init_metadata
-
+  after_destroy :destroy_metadata
 
   # todo add more default fields here
   def init_metadata
     ProjectMetadata.create(project_id: self.id)
+  end
+
+  def destroy_metadata
+    ProjectMetadata.where(project_id: self.id).first.destroy
   end
 
   def project_metadata
