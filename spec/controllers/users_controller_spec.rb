087919a3dd5fd33504d3e557b3fcce9c3f48edfa
Miles Frankel <miles.frankel@gmail.com>
2016-10-16 18:23:11 -0400

    factorygirl + user ctrl spec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,23 +2,27 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
 
-  let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
-  }
-
-  let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
-  }
-
   let(:valid_session) {
-    {}
+    {} # this controller doesn't need any session stuff yet
   }
+
+
+  let(:rbody) { JSON.parse(response.body) }
 
   describe "GET #index" do
-    it "assigns all delete_mes as @delete_mes" do
-      user = User.create! valid_attributes
+    it "returns all users" do
+      users = create_list(:user, 5)
+      # stub the model method
+      allow(User).to receive(:all).and_return(users)
       get :index, params: {}, session: valid_session
-      expect(assigns(:users)).to eq([user])
+      expect(response).to be_success
+      # ensure all models are returned
+      expect(rbody.length).to equal(5)
+      # ensure all keys are preserved
+      rbody.each do |h|
+        expect(h.keys).to match_array(users[0].as_json.keys)
+      end
+
     end
   end
 
