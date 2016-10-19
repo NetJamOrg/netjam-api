4a81b0bb757e26a3aeb5e6501b73bf689f1806fa
Miles Frankel <miles.frankel@gmail.com>
2016-10-18 20:46:01 -0400

    progress

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,42 @@
 class ApplicationController < ActionController::API
 
-  def current_user
-    session[:current_user]
+  HMAC_SECRET = ENV['JWT_HMAC_SECRET']
+
+  def current_user_hash
+    byebug
+    token = request.headers['Authorization'].sub(/^Bearer /, '') # remove leading 'Bearer'
+    begin
+      decoded_token = JWT.decode token, HMAC_SECRET, true, { algorithm: 'HS256' }
+    rescue JWT::ExpiredSignature
+      render json: { error: 'Session has expired' }, status: 403
+    rescue JWT::DecodeError => e
+      # https://github.com/jwt/ruby-jwt/blob/master/lib/jwt/error.rb
+      head :unauthorized
+      return nil
+    end
+    byebug
+    # Array
+    # [
+    #   {"data"=>"test"}, # payload
+    #   {"typ"=>"JWT", "alg"=>"none"} # header
+    # ]
+    # TODO check against logged_out db
+    payload = decoded_token[0]
+    return payload['user']
   end
 
   def user_authorized?
-    current_user&.id != nil
+    byebug
+    token = request.headers['Authorization'].sub(/^Bearer /, '') # remove leading 'Bearer'
+    begin
+      # just check decode successful
+      JWT.decode token, HMAC_SECRET, true, { algorithm: 'HS256' }
+    rescue JWT::DecodeError => e
+      # https://github.com/jwt/ruby-jwt/blob/master/lib/jwt/error.rb
+      head :unauthorized
+      return false
+    end
+    return true
   end
 
   def ensure_user_authorized
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,12 @@
 # This class facilitates oauth2 authentication. successful oauth callbacks redirect to #create,
 # where the session is created with the information of the associated user in our db (which is created if necessary).
 
+require 'jwt'
+require 'dalli'
+
 class SessionsController < ApplicationController
+
+  HMAC_SECRET = ENV['JWT_HMAC_SECRET']
 
   def create
     @user = User.where(provider: auth_hash[:provider], oauth_uid: auth_hash[:uid]).first
@@ -9,8 +14,15 @@ class SessionsController < ApplicationController
       @user = User.create(provider: auth_hash[:provider], oauth_uid: auth_hash[:uid], name: auth_hash[:info][:name], email: auth_hash[:info][:email], username: auth_hash[:info][:email])
     end
     if @user&.id != nil
-      session[:current_user] = @user
-      render json: @user
+      byebug
+      # create and return a json web token. disregard any session bullshit
+      payload = { user: @user.as_json, exp: (DateTime.now + 5.minutes).to_i }
+      token = JWT.encode payload, HMAC_SECRET, 'HS256'
+      # delete session_id cookie leftover from oauth
+      # cookies.delete
+      byebug
+      # return jwt
+      render json: token
     else
       head :not_found
     end
@@ -18,7 +30,8 @@ class SessionsController < ApplicationController
 
 
   def destroy
-    session[:current_user] = @user = nil
+    # TODO: add token to a different memcache namespace with expires = tok.valid_until, data: {disabled: true}
+    # and check this in app controller is valid etc
   end
 
   protected def auth_hash
